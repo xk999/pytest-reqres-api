720ff59f87fe2ccad5b26576b39d5ae090d140db
Xenia Klepikova <83287741+xk999@users.noreply.github.com>
2022-08-25 13:51:33 +0500

    Create test_login2.py

---
+++ test_login2.py
@@ -0,0 +1,15 @@
+import pytest
+import requests
+from payloads import *
+
+login_link = "https://reqres.in/api/login"
+
+#check POST response for valid data
+def test_login():
+    resp = requests.post(login_link, data = valid_login)
+    assert resp.status_code == 201, f"Error: got status code {resp.status_code} instead of 200"
+
+#check POST response for invalid data
+@pytest.mark.parametrize("test_data", invalid_login)
+def test_invalid_login(test_data):
+    assert requests.post(login_link, test_data).status_code in range(400,499), f"This invalid data was processed as valid: {test_data}"
